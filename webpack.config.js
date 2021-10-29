6f430ff7759451aecad188b5fc657b74f0becbe8
Matt Di Micelli <matthewdimicelli@gmail.com>
2021-10-29 13:26:36 -0400

    set up tailwindcss and webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',  //Injects the CSS into the DOM
+          {
+            loader: 'css-loader',  // Reads CSS from CSS files, ensures that imports & exports are parsed correctly
+            options: {
+              importLoaders: 1
+            }
+          },
+          'postcss-loader'
+        ]
+      },
+    ]
+  }, 
+  devServer: {
+    static: './dist',
+    open: true,
+  }
+};
+
+module.exports = config;
