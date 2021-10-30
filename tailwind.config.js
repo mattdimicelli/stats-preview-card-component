0168fbdcb3e678a0fba8e5afa7cb8c4bc8324260
Matt Di Micelli <matthewdimicelli@gmail.com>
2021-10-29 20:45:54 -0400

    continue to style

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,9 +17,23 @@ module.exports = {
       }
     },
     fontFamily: {
-      'Inter': ['Inter', 'sans-serif'],
-      'Lexend Deca': ['Lexend Deca', 'sans-serif'],
-    }
+      'Inter': ['"Inter"', 'sans-serif'],
+      'Lexend-Deca': ['"Lexend Deca"', 'sans-serif'],
+    },
+    fontSize: {
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'tiny': '.938rem',
+       'base': '1rem',
+       'lg': '1.125rem',
+       'xl': '1.25rem',
+       '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+       '5xl': '3rem',
+       '6xl': '4rem',
+      '7xl': '5rem',
+     }
   },
   variants: {
     extend: {},
